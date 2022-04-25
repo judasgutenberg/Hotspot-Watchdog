4b1231c8bd48f9ea0d8153212d3f0dd3ab4923fe
Gus Mueller <gus@asecular.com>
2022-04-24 21:30:48 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+
+<?php
+
+$servername = "localhost";
+$username = "your_sql_username";
+$database = "your_sql_database";
+$password = "your_sql_password";
+$storagePassword = "your_storage_password"; //should match value in arduino sketch
