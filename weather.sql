05eb8e79ed8fe8e46f537d60f7e31bad613b71fe
Gus Mueller <gus@asecular.com>
2022-04-24 21:32:17 -0400

    Create weather.sql

---
+++ weather.sql
@@ -0,0 +1,15 @@
+
+
+CREATE TABLE weather_data(
+weather_data_id INT AUTO_INCREMENT PRIMARY KEY,
+location_id INT NULL,
+recorded DATETIME,
+temperature DECIMAL(6,3) NULL,
+pressure DECIMAL(9,4) NULL,
+humidity DECIMAL(6,3) NULL,
+wind_direction INT NULL,
+precipitation INT NULL,
+wind_speed DECIMAL(8,3) NULL,
+wind_increment INT NULL,
+gas_metric DECIMAL(9,4) NULL
+)
